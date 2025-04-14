64663c4de3269d5b06c5b81123388138d63834c4
danya <danyakresichenko0709@gmail.com>
2025-04-14 09:41:54 +0300

    Dockerfile backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,10 +2,6 @@ cd /app/backend
 
 python3 manage.py --prod migrate --no-input
 
-mkdir "static"
-
-python3 manage.py --prod collectstatic
-
 gunicorn --workers=4 --bind 0.0.0.0:8000 devart.wsgi --prod &
 
 celery -A --prod devart worker --loglevel=info
